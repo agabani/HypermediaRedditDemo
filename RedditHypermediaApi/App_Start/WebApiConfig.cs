f9cc85da01a6016bff7dbf6d3ec13d5ed864f8f6
Ahmed Agabani <agabani@live.com>
2016-08-09 01:31:16 +0100

    Enabled cors

--- RedditHypermediaApi/App_Start/WebApiConfig.cs
+++ RedditHypermediaApi/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Http;
+using System.Web.Http.Cors;
 using FluentSiren.AspNet.WebApi.Formatting;
 
 namespace RedditHypermediaApi
@@ -7,6 +8,8 @@ namespace RedditHypermediaApi
     {
         public static void Register(HttpConfiguration configuration)
         {
+            configuration.EnableCors(new EnableCorsAttribute("*", "*", "*"));
+
             configuration.Formatters.Add(new SirenMediaFormatter());
 
             configuration.MapHttpAttributeRoutes();
